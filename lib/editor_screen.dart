de166f7f2d6e1e711090e13b11ff06eba5ab03bc
raazi-ak <raazi.faisal@gmail.com>
2024-02-21 11:58:42 +0530

    latest commit

---
+++ lib/editor_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:image_editor_plus/image'
--- lib/image_card.dart
+++ lib/image_card.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ImageCard extends StatelessWidget {
-  const ImageCard({super.key});
+class ImageCard extends StatefulWidget {
+  const ImageCard({Key? key}) : super(key: key);
+
+  @override
+  State<ImageCard> createState() => _ImageCardState();
+}
+
+class _ImageCardState extends State<ImageCard> {
+  File? image;
+
+  Future<void> pickImage() async {
+    try {
+      final pickedImage =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (pickedImage == null) return;
+      final imageTemp = File(pickedImage.path);
+      setState(() {
+        image = imageTemp;
+      });
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +38,19 @@ class ImageCard extends StatelessWidget {
         ),
         Center(
           child: GestureDetector(
-            onTap: () {},
+            onTap: pickImage,
             child: Container(
-                width: 0.8 * (MediaQuery.of(context).size.width),
-                height: 0.6 * (MediaQuery.of(context).size.height),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                  color: Color(0xFF25251f),
-                ),
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
+              width: 0.8 * (MediaQuery.of(context).size.width),
+              height: 0.6 * (MediaQuery.of(context).size.height),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                color: Color(0xFF25251f),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (image == null) ...[
                       const Icon(
                         Icons.add_circle_outline_rounded,
                         size: 86.0,
@@ -44,9 +69,16 @@ class ImageCard extends StatelessWidget {
                           color: const Color.fromARGB(255, 58, 58, 36),
                         ),
                       )
-                    ],
-                  ),
-                )),
+                    ] else
+                      Image.file(
+                        image!,
+                        width: 0.8 * (MediaQuery.of(context).size.width),
+                        height: 0.6 * (MediaQuery.of(context).size.height),
+                      ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ],
